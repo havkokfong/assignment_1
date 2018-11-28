762e840cdb60a4b317818ba0ac4a57ba7623ca02
Hav Kokfong <havkokfong@my.jcu.edu.au>
2018-11-28 14:09:31 +0800

    Define main function

--- assessment_1.py
+++ assessment_1.py
@@ -2,11 +2,18 @@
 Replace the contents of this module docstring with your own details.
 """
 
-MENU = input("L")
+MENU = input("L - List songs\nA - Add new song\nC - Complete a song\nQ - Quit\n\nPlease select your choices:\n>>> ").upper()
 
 def main():
-    print("Songs To Learn 1.0 - by <Your Name>")
+    print("Songs To Learn 1.0 - by <Kokfong Hav>")
+    print(MENU)
+    while MENU != Q:
+        if MENU == L:
+            print()
+    else:
+        exit()
 
 
-if __name__ == '__main__':
-    main()
+
+#if __name__ == '__main__':
+main()
